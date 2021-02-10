71030a261b19b2510a0427b31a2c3632cb21d0da
04rookie <47923475+04rookie@users.noreply.github.com>
2021-02-10 19:50:20 +0530

    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function App() {
+  setInterval(fetch, 1000);
+  const [time, getTime] = React.useState(new Date().toLocaleTimeString());
+
+  function fetch() {
+    getTime(new Date().toLocaleTimeString());
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={fetch}>Get Time</button>
     </div>
   );
 }
